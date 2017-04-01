2f31cc3b0c7b248c9d145c9fd47595a41b27b78e
Purushottam Abhisheikh <purushottamabhisheikh@gmail.com>
2017-04-01 18:35:06 +0530

    adding splash screen

---
+++ SIHApp/app/src/main/java/com/example/abhisheikh/sihapp/activity/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.abhisheikh.sihapp.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.abhisheikh.sihapp.R;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
