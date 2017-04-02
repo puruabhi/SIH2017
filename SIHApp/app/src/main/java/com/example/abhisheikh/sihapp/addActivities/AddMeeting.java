9a4e4604eb2b5b1a026135fc74b1c2f4ce12514a
kmlkant9 <kmltechnomagi@gmail.com>
2017-04-02 10:35:11 +0530

    Meeting Authentication added

--- SIHApp/app/src/main/java/com/example/abhisheikh/sihapp/addActivities/AddMeeting.java
+++ SIHApp/app/src/main/java/com/example/abhisheikh/sihapp/addActivities/AddMeeting.java
@@ -1,31 +1,60 @@
 package com.example.abhisheikh.sihapp.addActivities;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.abhisheikh.sihapp.R;
 import com.example.abhisheikh.sihapp.other.Meeting;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.util.ArrayList;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class AddMeeting extends AppCompatActivity {
 
     EditText dateEdittext,sdpEditText,descriptionEditText,statusEditText;
     Calendar myCalendar;
     DatePickerDialog.OnDateSetListener date;
-    Button addButton;
+    Button addButton, btpic;
+    ImageView mImageView;
+    String mCurrentPhotoPath, ba1;
+    public static String URL = "http://192.168.137.23/imgupload/practice.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +68,16 @@ public class AddMeeting extends AppCompatActivity {
         statusEditText=(EditText)findViewById(R.id.statusEditText);
         addButton  = (Button)findViewById(R.id.addMeetingButton);
         myCalendar = Calendar.getInstance();
+
+        //capture image
+        mImageView = (ImageView) findViewById(R.id.Imageprev);
+        btpic = (Button) findViewById(R.id.cpic);
+        btpic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                captureImage();
+            }
+        });
 
         dateEdittext.setText(updateLabel());
         date = new DatePickerDialog.OnDateSetListener() {
@@ -64,7 +103,7 @@ public class AddMeeting extends AppCompatActivity {
                 String sdp = sdpEditText.getText().toString();
                 String description = descriptionEditText.getText().toString();
                 String status= statusEditText.getText().toString();
-                System.out.println("sdp is:"+sdp);
+                upload();
                 if(date!=null && !sdp.equals("")) {
                     Intent intent = new Intent();
                     intent.putExtra("date", date);
@@ -91,6 +130,127 @@ public class AddMeeting extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void upload() {
+        Bitmap bm = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 50, bao);
+        byte[] ba = bao.toByteArray();
+        ba1 = Base64.encodeToString(ba,Base64.NO_WRAP);
+
+
+        // Upload image to server
+        new uploadToServer().execute();
+
+    }
+
+    private void captureImage() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, 100);
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            setPic();
+        }
+    }
+
+    public class uploadToServer extends AsyncTask<Void, Void, String> {
+
+//        private ProgressDialog pd = new ProgressDialog(AddMeeting.this);
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            pd.setMessage("Wait image uploading!");
+//            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("base64", ba1));
+            nameValuePairs.add(new BasicNameValuePair("ImageName", System.currentTimeMillis() + ".jpg"));
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(URL);
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                String st = EntityUtils.toString(response.getEntity());
+                Log.v("log_tag", "In the try Loop" + st);
+
+            } catch (Exception e) {
+                Log.v("log_tag", "Error in http connection " + e.toString());
+            }
+            return "Success";
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+//            pd.hide();
+//            pd.dismiss();
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.e("Getpath", "Cool" + mCurrentPhotoPath);
+        return image;
     }
 
     private String updateLabel(){
